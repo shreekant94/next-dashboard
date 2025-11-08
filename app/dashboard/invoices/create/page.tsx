6555572f83b5c88261cec7b43683c9cdd9018d2d
shreekant <shreey94@gmail.com>
2025-11-08 23:31:34 +0530

    removed uncached error

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,10 +1,15 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
-export default async function Page() {
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+
+async function CreateInvoiceForm() {
   const customers = await fetchCustomers();
- 
+  return <Form customers={customers} />;
+}
+
+export default function Page() {
   return (
     <main>
       <Breadcrumbs
@@ -17,7 +22,9 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} />
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CreateInvoiceForm />
+      </Suspense>
     </main>
   );
 }
